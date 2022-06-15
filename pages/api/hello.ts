127f6f1b3846722abdb1c10f599d89b595e08ffb
Sabiq Muhammad Antebing Mame <72848275+sabiq7392@users.noreply.github.com>
2022-06-16 02:57:09 +0700

    success to save file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,14 +9,46 @@ type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
+  // res: NextApiResponse<Data>
 ) {
     
   try {
-    const process = await new ffmpeg(path.resolve("public/videos/mp4", "city.mp4"));
-    const object = process.metadata;
+    const process = new ffmpeg(path.resolve("public/videos/mp4", "city.mp4"));
 
-    res.status(200).json({ data: object  })
+    process.then(function (video) {
+      // Callback mode
+      video.fnExtractFrameToJPG('C:/xampp/htdocs/__coding__/convert-video/public/', {
+        frame_rate : 1,
+        number : 5,
+        file_name : 'my_frame_%t_%s'
+      }, function (error, files) {
+        if (!error)
+          console.log('Frames: ' + files);
+      });
+    }, function (err) {
+      console.log('Error: ' + err);
+    });
+    
+    // process.then(video => {
+    //   video
+    //     .save('C:\\xampp\\htdocs\\__coding__\\convert-video\\public\\', (error, file) => {
+    //       if (error) {
+    //         console.log(error);
+    //       } else {
+    //         console.log(file)
+    //       }
+    //     })
+    // })
+      // .setVideoSize('640x?', true, true, '#fff')
+      // .setAudioCodec('libfaac')
+      // .setAudioChannels(2)
+      // .setVideoFormat('webm')
+      // .setVideoFrameRate(30)
+      
+    
+
+    return res.status(200).json({ data: (await process).metadata  })
   } catch (error) {
     console.error({ error });
   }
